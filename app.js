1f5e86bcf1edce315bb70886f8f2baef73b7c8bd
Chris Cuellar <chris@civil-services.com>
2013-08-14 18:56:30 -0700

    added mapbox rail line selectors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var $messages = $('#messages'),
 	$disciplines = $('.checkbox input'),
 	$railLines = $('#rail-lines'),
-	tableName = "cultural_asset_list_2",
+	//tableName = "cultural_asset_list",
+	//cartoViz = "http://cs-chris.cartodb.com/api/v2/viz/7aaa7842-eb50-11e2-ab8e-61f262e10947/viz.json",
+	tableName = "lan_data_set",
+	cartoViz = "http://cs-chris.cartodb.com/api/v2/viz/1c721cc6-0545-11e3-8ab0-796eb9e49271/viz.json",
 	filters = {};
 
 function serialize(filters, layer) {
@@ -60,7 +63,7 @@ function createSelector(layer) {
 
 	/*standard select filter from cartoDB*/
 	$('.js-selectFilter').change(function(e) {
-		
+
 		/*show child-filter, if any*/		
 		var filter = $("option:selected",this)[0].dataset;
 	    	'target' in filter ? $('#' + filter.target).removeClass('u-isHiddenVisually') : 
@@ -83,37 +86,37 @@ function createSelector(layer) {
 
 }
 
+/*instantiate map and add  base layers*/
  var map = new L.Map('map', {
-		center: [34.056,-118.235],
-		zoom: 11
-	});
+	center: [34.056,-118.235],
+	zoom: 11
+});
 
-	L.tileLayer('http://a.tiles.mapbox.com/v3/examples.map-zgrqqx0w/{z}/{x}/{y}.png', {
-					attribution: 'Mapbox'
-				}).addTo(map);
+L.tileLayer('http://a.tiles.mapbox.com/v3/examples.map-zgrqqx0w/{z}/{x}/{y}.png', {
+	attribution: 'Mapbox'
+}).addTo(map);
 
-	var railLines = L.tileLayer('http://a.tiles.mapbox.com/v3/civservchris.metroLines/{z}/{x}/{y}.png', {
-					attribution: 'Mapbox'
-				});
-	railLines.addTo(map);
+var railLines = L.tileLayer('http://a.tiles.mapbox.com/v3/civservchris.metroLines/{z}/{x}/{y}.png', {
+	attribution: 'Mapbox'
+});
+railLines.addTo(map);
 
-var markerLayer = cartodb.createLayer(map, 'http://cs-chris.cartodb.com/api/v2/viz/7aaa7842-eb50-11e2-ab8e-61f262e10947/viz.json');
+
+/*rail line selector*/
+$('#rail-lines').change(function() {
+	var filter = $("option:selected",this)[0].dataset;
+	var url = "http://a.tiles.mapbox.com/v3/civservchris.metroLines/{z}/{x}/{y}.png";
+	if ( "mblayer" in filter)  url = "http://a.tiles.mapbox.com/v3/civservchris."+filter.mblayer+"/{z}/{x}/{y}.png" ;
+	railLines.setUrl( url );
+});
+
+/*load cartoDB markers*/
+var markerLayer = cartodb.createLayer(map, cartoViz);
 markerLayer.addTo(map)
 	.on('done', function(layer) {
 			layer.setInteraction(true);
 			createSelector(layer);
 	});
 
-$('#rail-lines').change(function() {
-	var searchTerm = $("option:selected",this).val();
-	var url;
-	if ( searchTerm == "any") {
-		console.log(searchTerm);
-		url = 'http://a.tiles.mapbox.com/v3/civservchris.metroLines/{z}/{x}/{y}.png';
-		$('#rail-stops').css('display','none');
-	} else {
-		var url = "http://a.tiles.mapbox.com/v3/civservchris."+searchTerm+"/{z}/{x}/{y}.png";
-		$('#rail-stops').css('display','block');
-	}
-	railLines.setUrl( url );
-});
+
+
